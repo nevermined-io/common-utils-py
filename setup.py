88f2bd15dcf651b31db0b4aac825cc179c4d96b8
Aitor <1726644+aaitor@users.noreply.github.com>
2020-05-13 14:57:06 +0200

    Bump version: 0.2.6 → 0.2.7

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/keyko-io/common-utils-py',
-    version='0.2.6',
+    version='0.2.7',
     zip_safe=False,
 )
