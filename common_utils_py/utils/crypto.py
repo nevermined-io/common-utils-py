46e690400fab2215a60d299d8f2197b27da144ed
Aitor <1726644+aaitor@users.noreply.github.com>
2020-05-13 12:35:38 +0200

    adding AES encryption & decryption + RSA support for long messages

--- common_utils_py/utils/crypto.py
+++ common_utils_py/utils/crypto.py
@@ -1,10 +1,19 @@
 """Crypto Utils class"""
+import base64
 
 import rsa
+from Crypto import Random
+from Crypto.Cipher import AES
+from Crypto.Protocol.KDF import PBKDF2
 from ecies import encrypt, decrypt
 from eth_keys import keys
 from eth_utils import to_hex
 from web3.auto import w3
+
+
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 
 def get_keys_from_file(keyfile_path, keyfile_password):
@@ -76,11 +85,42 @@ def get_rsa_private_key_from_file(keyfile_path):
 
 
 def rsa_encryption(public_key, data):
-    return rsa.encrypt(data, public_key)
+    aes_key = rsa.randnum.read_random_bits(128)
+    encrypted_data = aes_encryption(data, aes_key)
+    encrypted_aes_key = rsa.encrypt(aes_key, public_key)
+    return encrypted_data, encrypted_aes_key
+    # return rsa.encrypt(data, public_key)
 
 
-def rsa_decryption(private_key, encrypted_data):
-    return rsa.decrypt(encrypted_data, private_key)
+def rsa_decryption(private_key, encrypted_data, encrypted_aes_key):
+    aes_key = rsa.decrypt(encrypted_aes_key, private_key)
+    return aes_decryption(encrypted_data, aes_key)
+    # return rsa.decrypt(aes_key, private_key)
+    # return rsa.decrypt(encrypted_data, private_key)
+
+
+def get_aes_private_key(passphrase):
+    salt = b'this is a salt'
+    kdf = PBKDF2(passphrase, salt, 64, 1000)
+    key = kdf[:32]
+    return key
+
+
+def aes_encryption(data, passphrase):
+    data = data.decode()
+    private_key = get_aes_private_key(passphrase)
+    data = pad(data).encode()
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(data))
+
+
+def aes_decryption(encrypted_data, passphrase):
+    private_key = get_aes_private_key(passphrase)
+    encrypted_data = base64.b64decode(encrypted_data)
+    iv = encrypted_data[:16]
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(encrypted_data[16:]))
 
 
 def encryption(public_key_hex, data):
