7cdc5ad9eb632a99a06f1c2682280a11e994525a
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-01-08 19:10:06 +0100

    Bump version: 0.4.2 → 0.4.3

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/common-utils-py',
-    version='0.4.2',
+    version='0.4.3',
     zip_safe=False,
 )
