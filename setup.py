c074c115be778e25083cd96f1984d02d492c40ad
Rodolphe Marques <marques.rodolphe@gmail.com>
2021-12-01 11:31:25 +0100

    Use ServiceAgreement instead of Service for NFT_SALES
    - bump version to 0.7.9

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/common-utils-py',
-    version='0.7.8',
+    version='0.7.9',
     zip_safe=False,
 )
