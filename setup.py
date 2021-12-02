d93e83d56db62e9aa2f1f6fb57c75f950324df8a
Sami Mäkelä <mrsmkl@gmail.com>
2021-12-02 16:19:26 +0200

    update version

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/common-utils-py',
-    version='0.7.9',
+    version='0.7.4',
     zip_safe=False,
 )
