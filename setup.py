ea102a5a6f52007fea4a1c27e2b5e3554ef57af0
Rodolphe Marques <rodolphe@keyko.io>
2022-02-09 13:56:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requirements = [
 setup_requirements = ['pytest-runner', ]
 
 test_requirements = [
-    'contracts-lib-py==0.10.1',
+    'contracts-lib-py==0.11.0',
     'web3==5.13.0',
     'coverage',
     'docker',
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nevermined-io/common-utils-py',
-    version='0.9.1',
+    version='0.10.0',
     zip_safe=False,
 )
